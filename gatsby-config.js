eb771a6af8c5095fbb5ccf6346f23bc990101c39
stillnotdavid <stillnotdavid@me.com>
2023-02-12 17:46:55 -0800

    Updating resume

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,15 @@
 module.exports = {
   siteMetadata: {
     title: `David Le Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: '@mkitio/gatsby-theme-password-protect',
+      options: {
+        password: 'redvelvet',
+      },
+    },
+  ],
 };
