47819c7def6da977fafee8b075c0bae585cebfa3
stillnotdavid <stillnotdavid@me.com>
2023-02-12 17:51:53 -0800

    updating

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,5 @@ module.exports = {
     title: `David Le Portfolio`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [
-    'gatsby-plugin-sass',
-    {
-      resolve: '@mkitio/gatsby-theme-password-protect',
-      options: {
-        password: 'redvelvet',
-      },
-    },
-  ],
+  plugins: ['gatsby-plugin-sass'],
 };
